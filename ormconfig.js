19516f7183dec75550d1787db81f000b5e9a7d92
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-07-11 16:14:39 -0300

    chore(typeorm): setup the database connection
    Configured typeorm to behave differently based on the environment.

--- ormconfig.js
+++ ormconfig.js
@@ -1,16 +1,17 @@
-module.exports = {
+if (process.env.ENVIRONMENT === 'develop'){
+  entities = ["./src/modules/**/infra/typeorm/entities/*.ts"];
+  migrations = ["./src/shared/infra/typeorm/migrations/*.ts"]
+}
+else {
+  entities = ["./dist/modules/**/infra/typeorm/entities/*.js"]
+  migrations = ["./dist/shared/infra/typeorm/migrations/*.js"]
+}
+  module.exports = {
   "type": "postgres",
-  "host": "localhost",
-  "port": 5432,
-  "username": "postgres",
-  "password": "docker",
-  "database": "github-tagger",
-  "entities": [
-    "./src/modules/**/infra/typeorm/entities/*.ts"
-  ],
-  "migrations": [
-    "./src/shared/infra/typeorm/migrations/*.ts"
-  ],
+  "url": process.env.DATABASE_URL,
+
+  "entities": entities,
+  "migrations": migrations,
   "cli": {
     "migrationsDir": "./src/shared/infra/typeorm/migrations"
   }
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 
 import app from './app';
 
+import '@shared/infra/typeorm';
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening at port: ${process.env.PORT}`);
 });
