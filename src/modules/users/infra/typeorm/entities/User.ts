b31808ca345e59e0d2f086c8816836806705fdc6
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-07-12 04:08:24 -0300

    feat(user): user entity coded

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  github_token: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
