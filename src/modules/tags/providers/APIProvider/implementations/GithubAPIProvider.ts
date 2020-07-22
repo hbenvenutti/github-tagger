7e31423a1c1b9206c09f9f1416022e63d031a866
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-07-22 20:39:04 -0300

    fix(github-api-provider): could send a request with invalid github username
    fixed a bug where user could try to fetch repositories using a invalid github-username;
    now the code is inside a try-catch block;

--- src/modules/tags/providers/APIProvider/implementations/GithubAPIProvider.ts
+++ src/modules/tags/providers/APIProvider/implementations/GithubAPIProvider.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 import IGetStarredRepositoriesDTO from '@modules/tags/dtos/IGetStarredRepositoriesDTO';
+import AppError from '@shared/errors/AppError';
 import IAPIProvider from '../models/IAPIProvider';
 import IGithubResponseDTO from './dtos/IGithubResponseDTO';
 
@@ -14,28 +15,34 @@ class GithubAPIProvider implements IAPIProvider {
     githubToken?: string,
   ): Promise<IGetStarredRepositoriesDTO[]> {
     const authorization = githubToken || undefined;
+    try {
+      const response = await this.api.get(`/users/${username}/starred`, {
+        headers: {
+          authorization: `bearer ${authorization}`,
+        },
+      });
 
-    const response = await this.api.get(`/users/${username}/starred`, {
-      headers: {
-        authorization: `bearer ${authorization}`,
-      },
-    });
+      const githubRepositories = response.data as IGithubResponseDTO[];
 
-    const githubRepositories = response.data as IGithubResponseDTO[];
+      const repositories = githubRepositories.map(repo => {
+        const { id, name, description, html_url } = repo;
+        const repository = {
+          id,
+          name,
+          url: html_url,
+          description,
+        };
 
-    const repositories = githubRepositories.map(repo => {
-      const { id, name, description, html_url } = repo;
-      const repository = {
-        id,
-        name,
-        url: html_url,
-        description,
-      };
+        return repository;
+      });
 
-      return repository;
-    });
-
-    return repositories;
+      return repositories;
+    } catch {
+      throw new AppError(
+        'Request Failed: Did you provide the right github username?',
+        404,
+      );
+    }
   }
 }
 
