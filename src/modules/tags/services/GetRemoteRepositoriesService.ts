c79651afb840326af6aa4f5c3e50cdfa1370ad0b
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-07-22 19:49:00 -0300

    feat(get-remote-repos): service to fetch remote repositories

---
+++ src/modules/tags/services/GetRemoteRepositoriesService.ts
@@ -0,0 +1,35 @@
+import { injectable, inject } from 'tsyringe';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import AppError from '@shared/errors/AppError';
+import IAPIProvider from '../providers/APIProvider/models/IAPIProvider';
+import IGetStarredRepositoriesDTO from '../dtos/IGetStarredRepositoriesDTO';
+
+@injectable()
+class GetRemoteRepositoriesService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('GithubProvider')
+    private apiProvider: IAPIProvider,
+  ) {}
+
+  public async execute(id: string): Promise<IGetStarredRepositoriesDTO[]> {
+    const user = await this.usersRepository.findById(id);
+
+    if (!user) {
+      throw new AppError('User not found', 401);
+    }
+
+    const { username, github_token } = user;
+
+    const repositories = await this.apiProvider.getStarredRepositories(
+      username,
+      github_token,
+    );
+
+    return repositories;
+  }
+}
+
+export default GetRemoteRepositoriesService;
