096d5c50e2db7f90d928cbea9e070f2dab506bff
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-07-24 01:50:44 -0300

    feat(tsyringe): created a new container for repos-repository

--- src/modules/tags/providers/index.ts
+++ src/modules/tags/providers/index.ts
@@ -1,5 +1,12 @@
 import { container } from 'tsyringe';
 import IAPIProvider from './APIProvider/models/IAPIProvider';
 import GithubAPIProvider from './APIProvider/implementations/GithubAPIProvider';
+import IRepositoriesRepository from '../repositories/IRepositoriesRepository';
+import GithubReposRepository from '../infra/typeorm/repositories/GithubReposRepository';
 
 container.registerSingleton<IAPIProvider>('GithubProvider', GithubAPIProvider);
+
+container.registerSingleton<IRepositoriesRepository>(
+  'ReposRepository',
+  GithubReposRepository,
+);
